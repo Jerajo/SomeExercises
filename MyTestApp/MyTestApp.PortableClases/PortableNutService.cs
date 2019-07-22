69cfcd7c71411d41999352eff7cb42263f1c042d
Jesse R. Jose <jessejose@outlook.es>
2019-07-22 15:26:59 -0400

    NUT implemented on PCL

---
+++ MyTestApp/MyTestApp.PortableClases/PortableNutService.cs
@@ -0,0 +1,12 @@
+﻿using Nut;
+
+namespace MyTestApp.PortableClases
+{
+    public static class PortableNutService
+    {
+        public static string ToText(this int number)
+        {
+            return number.ToText("es");
+        }
+    }
+}
