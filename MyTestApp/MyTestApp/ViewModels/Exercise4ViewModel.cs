bac857bc6eda33928663fd97f254fae72206d84f
Jesse R. Jose <jessejose@outlook.es>
2019-07-22 07:05:13 -0400

    Exercise  4 Completed

---
+++ MyTestApp/MyTestApp/ViewModels/Exercise4ViewModel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Xamarin.Forms;
+using PropertyChanged;
+using Newtonsoft.Json;
+using MyTestApp.Models;
+using Newtonsoft.Json.Linq;
+using System.Windows.Input;
+using System.Collections.Generic;
+using MyTestApp.Models.Interfaces;
+using MyTestApp.ViewModels.Commands;
+
+namespace MyTestApp.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Exercise4ViewModel : BaseViewModel
+    {
+        public Exercise4ViewModel() : base()
+        {
+            Title = "Ejercicio 4";
+            IsOperationCreated = false;
+        }
+
+        #region Properties
+
+        public string NewFileName { get; set; }
+
+        public string JSONText { get; set; }
+
+        public bool IsOperationCreated { get; set; }
+
+        public JObject JSON { get; set; }
+
+        public OperationModel Operation { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand GenerateOperation
+        {
+            get => new DelegateCommand(new Action(GenerateOperationCommand));
+        }
+
+        public ICommand SaveJSON
+        {
+            get => new DelegateCommand(new Action(SaveJSONCommand));
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void GenerateOperationCommand()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                Operation = new OperationModel
+                {
+                    Instruccion = "Selecciona el resultado de la siguiente suma.",
+                    Problem = $"{Randomizer.Next(100000)}",
+                    Resoult = 0
+                };
+
+                InsertCorretOptions(Operation);
+
+                JSON = JObject.FromObject(Operation);
+                JSONText = JSON.ToString(Formatting.Indented);
+            }
+            catch (Exception ex)
+            {
+                await ShowError($"Error al generar la operación el archivo: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void SaveJSONCommand()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                if (ValidateNewFileName(NewFileName))
+                {
+                    IFileHelper service = DependencyService.Get<IFileHelper>();
+
+                    if (!NewFileName.Contains(".json"))
+                        NewFileName += ".json";
+
+                    await service?.WriteDocument(NewFileName, JSONText);
+                }
+                else
+                {
+                    string message = "";
+                    foreach (string error in Errors)
+                        message += error + "\n";
+
+                    await ShowAlert(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                await ShowError($"Error al guardar el archivo: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MyTestApp/MyTestApp/ViewModels/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyTestApp.ViewModels.Extensions
+{
+    public static class ListExtensions
+    {
+        private static Random randomizer = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int startIndex = list.Count;
+            while (startIndex > 1)
+            {
+                startIndex--;
+                int index = randomizer.Next(startIndex + 1);
+                T value = list[index];
+                list[index] = list[startIndex];
+                list[startIndex] = value;
+            }
+        }
+    }
+}
