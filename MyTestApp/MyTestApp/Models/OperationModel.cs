6fc8f191a36822cee285a511418a76d3d9301477
Jesse R. Jose <jessejose@outlook.es>
2019-07-22 07:04:01 -0400

    Exercise 2 Completed

---
+++ MyTestApp/MyTestApp/Models/OperationModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MyTestApp.Models
+{
+    public class OperationModel
+    {
+        public string Instruccion { get; set; }
+        public object Problem { get; set; }
+        public List<string> Options { get; set; }
+        public object Resoult { get; set; }
+    }
+}
---
+++ MyTestApp/MyTestApp/ViewModels/Exercise2ViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Xamarin.Forms;
+using PropertyChanged;
+using Newtonsoft.Json;
+using MyTestApp.Models;
+using Newtonsoft.Json.Linq;
+using System.Windows.Input;
+using System.Collections.Generic;
+using MyTestApp.Models.Interfaces;
+using MyTestApp.ViewModels.Commands;
+
+namespace MyTestApp.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Exercise2ViewModel : BaseViewModel
+    {
+        public Exercise2ViewModel() : base()
+        {
+            Title = "Ejercicio 2";
+            IsOperationCreated = false;
+            Operator = '+';
+        }
+
+        #region Properties
+
+        public string NewFileName { get; set; }
+
+        public char Operator { get; set; }
+
+        public string JSONText { get; set; }
+
+        public bool IsOperationCreated { get; set; }
+
+        public JObject JSON { get; set; }
+
+        public OperationModel Operation { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand GenerateOperation
+        {
+            get => new DelegateCommand(new Action(GenerateOperationCommand));
+        }
+
+        public ICommand SaveJSON
+        {
+            get => new DelegateCommand(new Action(SaveJSONCommand));
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void GenerateOperationCommand()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                Operation = new OperationModel
+                {
+                    Instruccion = "Selecciona el resultado de la siguiente suma.",
+                    Problem = new List<string>
+                    {
+                        Randomizer.Next(100000).ToString("n0"),
+                        Randomizer.Next(100000).ToString("n0")
+                    },
+                    Options = new List<string>
+                    {
+                        Randomizer.Next(100000).ToString("n0"),
+                        Randomizer.Next(100000).ToString("n0"),
+                        Randomizer.Next(100000).ToString("n0"),
+                        Randomizer.Next(100000).ToString("n0")
+                    },
+                    Resoult = 0
+                };
+
+                InsertCorretOptions(Operation, Operator);
+
+                JSON = JObject.FromObject(Operation);
+                JSONText = JSON.ToString(Formatting.Indented);
+            }
+            catch (Exception ex)
+            {
+                await ShowError($"Error al generar la operación el archivo: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async void SaveJSONCommand()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                if (ValidateNewFileName(NewFileName))
+                {
+                    IFileHelper service = DependencyService.Get<IFileHelper>();
+
+                    if (!NewFileName.Contains(".json"))
+                        NewFileName += ".json";
+
+                    await service?.WriteDocument(NewFileName, JSONText);
+                }
+                else
+                {
+                    string message = "";
+                    foreach (string error in Errors)
+                        message += error + "\n";
+
+                    await ShowAlert(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                await ShowError($"Error al guardar el archivo: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
+    }
+}
